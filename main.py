3004479d580cc00bbb51253cb56778793ab7cb85
soheyHashimoto <heyso1209@gmail.com>
2021-07-15 17:32:10 +0900

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-linebeacon.start_with_device_message("014ff0e715", "1a2b3c4d5e6f70809010a0b0c0")
+def on_button_pressed_a():
+    basic.show_icon(IconNames.SQUARE)
+    linebeacon.start_with_device_message("014ff0e715", "1a2b3c4d5e6f70809010a0b0c0")
+    linebeacon.stop()
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
+let v = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.Chessboard)
     linebeacon.startWithDeviceMessage(
     "014ff0e715",
     "1a2b3c4d5e6f70809010a0b0c0"
     )
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.clearScreen()
     linebeacon.stop()
+})
+input.onButtonPressed(Button.B, function () {
+    for (let i = 0; i <= 20; i++) {
+        v = i + 1
+        if (v % 3 == 0 && v % 5 == 0) {
+            basic.showString("Fizz Buzz")
+        } else {
+            if (v % 3 == 0) {
+                basic.showString("Fizz")
+            } else {
+                if (v % 5 == 0) {
+                    basic.showString("Buzz")
+                } else {
+                    basic.showNumber(v)
+                }
+            }
+        }
+    }
 })
